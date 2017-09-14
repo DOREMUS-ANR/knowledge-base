1faf7b4b8d4cbfd2e72e0cb148b9adf47ad162d9
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2017-09-14 17:20:19 +0200

    new dump of data

--- app/prefixes.sql
+++ app/prefixes.sql
@@ -3,3 +3,5 @@ DB.DBA.XML_SET_NS_DECL ('mus', 'http://data.doremus.org/ontology/#', 2);
 DB.DBA.XML_SET_NS_DECL ('efrbroo', 'http://erlangen-crm.org/efrbroo/', 2);
 DB.DBA.XML_SET_NS_DECL ('ecrm', 'http://erlangen-crm.org/current/', 2);
 DB.DBA.XML_SET_NS_DECL ('doremus', 'http://data.doremus.org/', 2);
+DB.DBA.XML_SET_NS_DECL ('modsrdf', 'http://www.loc.gov/standards/mods/rdf/v1/#', 2);
+DB.DBA.XML_SET_NS_DECL ('dcat', 'http://www.w3.org/ns/dcat#', 2);
