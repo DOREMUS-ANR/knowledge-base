c635397f8445bd06bf6c982d41159d634517f164
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2017-09-25 15:54:29 +0200

    some alt labels are pref labels

--- app/prefixes.sql
+++ app/prefixes.sql
@@ -5,3 +5,4 @@ DB.DBA.XML_SET_NS_DECL ('ecrm', 'http://erlangen-crm.org/current/', 2);
 DB.DBA.XML_SET_NS_DECL ('doremus', 'http://data.doremus.org/', 2);
 DB.DBA.XML_SET_NS_DECL ('modsrdf', 'http://www.loc.gov/standards/mods/rdf/v1/#', 2);
 DB.DBA.XML_SET_NS_DECL ('dcat', 'http://www.w3.org/ns/dcat#', 2);
+DB.DBA.XML_SET_NS_DECL ('time', 'http://www.w3.org/2006/time#', 2);
