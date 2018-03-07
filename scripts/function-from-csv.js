60ae94a0970d774570827ffe0d04ca381c5c9d6f
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2018-03-07 18:18:22 +0100

    function voc update

--- scripts/function-from-csv.js
+++ scripts/function-from-csv.js
@@ -5,6 +5,15 @@ const fs = require('fs'),
   rdfTranslator = require('rdf-translator'),
   $rdf = require('rdflib');
 
+const CONTEXT = "Contexte d'utilisation (à mettre en scopenote)";
+const SKOS_CONTEXT = "Contexte (collection skos)";
+
+const DIVIDER = '\\n';
+const DIVIDER_REGEX = /\\n/g;
+
+const COLLECTION_LIST = ['performance', 'casting', 'musical_work', 'creative_work', 'recording', 'publication', 'performance_plan', 'editing'];
+const BASE_URL = 'http://data.doremus.org/vocabulary/function/';
+
 const RDF = $rdf.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
 const SKOS = $rdf.Namespace("http://www.w3.org/2004/02/skos/core#");
 const RDFS = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#");
@@ -21,8 +30,12 @@ store.setPrefixForURI('skos', 'http://www.w3.org/2004/02/skos/core#');
 store.setPrefixForURI('dct', 'http://purl.org/dc/terms/');
 store.setPrefixForURI('xsd', 'http://www.w3.org/2001/XMLSchema#');
 
-var baseUrl = 'http://data.doremus.org/vocabulary/function/';
-var conceptScheme = $rdf.sym(baseUrl);
+COLLECTION_LIST.forEach(c => {
+  var collection = $rdf.sym(BASE_URL + c);
+  store.add(collection, RDF('type'), SKOS('Collection'));
+});
+
+var conceptScheme = $rdf.sym(BASE_URL);
 store.add(conceptScheme, RDF('type'), SKOS('ConceptScheme'));
 store.add(conceptScheme, RDFS('label'), $rdf.literal('List of functions', 'en'));
 store.add(conceptScheme, RDFS('label'), $rdf.literal('Liste des fonctions', 'fr'));
@@ -37,10 +50,11 @@ var curConcept = {}; // for saving the current concepts at different levels
 fs.createReadStream(file)
   .pipe(csv({
     separator: ';', // specify optional cell separator
-    newline: '\r' // specify a newline character
+    newline: '\n' // specify a newline character
   }))
   .on('data', (data) => {
     ++i;
+    // console.log(Object.keys(data));
 
     let labelEn = (data['prefLabel@en'] || '').trim();
     let labelFr = data['prefLabel@fr'].trim();
@@ -52,39 +66,49 @@ fs.createReadStream(file)
 
     console.log(labelFr);
 
-    let otherLabelEn = labelEn.split(/[\n\r]/);
+    let otherLabelEn = labelEn.split(DIVIDER_REGEX);
     labelEn = otherLabelEn.shift().trim();
 
-    let concept = $rdf.sym((baseUrl + (labelEn || i)).replace(/ /g, '_'));
+    let concept = $rdf.sym((BASE_URL + (labelEn || i)).replace(/ /g, '_'));
     curConcept[level] = concept;
     store.add(concept, RDF('type'), SKOS('Concept'));
-    store.add(concept, SKOS('prefLabel'), $rdf.literal(labelFr, 'fr'));
-
-    if (labelEn) {
-      store.add(concept, SKOS('prefLabel'), $rdf.literal(labelEn, 'en'));
-      for (let l of otherLabelEn) {
-        store.add(concept, SKOS('prefLabel'), $rdf.literal(l.trim(), 'en'));
-      }
-    }
+    addLabel(store, concept, labelFr, 'fr');
+    addLabel(store, concept, labelEn, 'en');
+    addLabel(store, concept, data['prefLabel@it'], 'it');
+    addLabel(store, concept, data['altLabel@en'], 'en', 'alt');
+    addLabel(store, concept, data['altLabel@fr'], 'fr', 'alt');
+    addLabel(store, concept, data['altLabel@it'], 'it', 'alt');
 
     if (level === 0)
       store.add(concept, SKOS('topConceptOf'), conceptScheme);
     else
       store.add(concept, SKOS('broader'), curConcept[level - 1]);
+
+    for (let ctx of data[CONTEXT].split(DIVIDER)) {
+      ctx = ctx.trim();
+      if (!ctx) continue;
+      store.add(concept, SKOS('scopeNote'),
+        $rdf.literal("Contexte d'utilisation: " + ctx.trim(), 'fr'));
+    }
+
+    for (let ctx of data[SKOS_CONTEXT].split(DIVIDER)) {
+      ctx = ctx.trim();
+      if (!ctx) continue;
+      store.add($rdf.sym(BASE_URL + ctx),
+        SKOS('member'), concept);
+    }
   })
   .on('end', () => {
     // print modified ttl document
     $rdf.serialize(undefined, store, 'http://example.org/', 'text/turtle', function(err, str) {
       if (err) return console.error(err);
-      str = str.replace(`@prefix fun: <${baseUrl}>.`, '');
-      str = str.replace(/fun:([^\s;.]*)/g, `<${baseUrl}$1>`);
+      str = str.replace(`@prefix fun: <${BASE_URL}>.`, '');
+      str = str.replace(/fun:([^\s;,.]*)/g, `<${BASE_URL}$1>`);
       // return writeTtl(str);
 
-      rdfTranslator(str, 'n3', 'n3', function(err, data) {
-        if (err) return console.error(err);
-
-        writeTtl(data);
-      });
+      rdfTranslator(str, 'n3', 'n3')
+        .then(writeTtl)
+        .catch(e => console.error(e));
     });
 
   });
@@ -103,4 +127,21 @@ function dateToYMD(date) {
   var m = date.getMonth() + 1; //Month from 0 to 11
   var y = date.getFullYear();
   return y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+}
+
+function addLabel(store, concept, label = '', lang = null, type = 'pref') {
+  if (Array.isArray(label))
+    return label
+      .forEach(l => addLabel(store, concept, l, lang, type));
+
+  label = label.trim();
+  if (!label) return;
+
+  if (label.includes(DIVIDER))
+    return label.split(DIVIDER_REGEX)
+      .forEach(l => addLabel(store, concept, l, lang, type));
+
+
+  let literal = lang ? $rdf.literal(label, lang) : label;
+  store.add(concept, SKOS(`${type}Label`), literal);
 }
